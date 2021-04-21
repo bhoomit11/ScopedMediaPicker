a3de5733deecfe7e72dcaf0bfb0ee9b258ee2c0b
Bhumit Belani <bhumit.belani@hiddenbrains.in>
2021-04-21 14:10:03 +0530

    AD commit

--- app/src/main/java/com/media/picker/MediaPickerActivity.kt
+++ app/src/main/java/com/media/picker/MediaPickerActivity.kt
@@ -1,30 +1,12 @@
 package com.media.picker
 
-import android.Manifest
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.os.Parcelable
-import android.provider.MediaStore
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.media.scopemediapicker.ScopedImagePicker
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.File
 
 class MediaPickerActivity : AppCompatActivity() {
 
